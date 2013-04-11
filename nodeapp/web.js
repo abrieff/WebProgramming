43e806438f6a31c5c2bb057db2d3b9aee6b65d3c
Ming Chow <mchow@cs.tufts.edu>
2013-04-11 16:35:22 -0400

    Updated nodeapp p with proper code to connect to a MongoDB

--- nodeapp/web.js
+++ nodeapp/web.js
@@ -4,7 +4,20 @@ var app = express(express.logger());
 app.use(express.bodyParser());
 app.set('title', 'nodeapp');
 
+// Mongo initialization
+var mongoUri = process.env.MONGOLAB_URI || 
+  process.env.MONGOHQ_URL || 
+  'mongodb://localhost/scorecenter';
+var mongo = require('mongodb');
+var db = mongo.Db.connect(mongoUri, function (error, databaseConnection) {
+	db = databaseConnection;
+});
+
 app.get('/', function (request, response) {
+	/*
+		db.collection('NAME_OF_YOUR_COLLECTON_HERE...', function(er, collection) {
+			collection.find()...
+	*/
 	response.set('Content-Type', 'text/html');
 	response.send('<p>Hi!</p>');
 });
